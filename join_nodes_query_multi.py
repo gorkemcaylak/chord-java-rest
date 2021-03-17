0d1754359ef42b8d38df041ce94d4ccd20aaec9a
Gorkem Caylak <gorkem.caylak@gmail.com>
2021-03-17 16:15:06 -0700

    calculating mean median stddev - intermittent problem when running find successors

--- join_nodes_query_multi.py
+++ join_nodes_query_multi.py
@@ -4,58 +4,68 @@ import random
 import time
 import pickle
 import requests
+import statistics
 
-CHORD_SIZE = 128
-TOTAL_NODES = 30
-QUERY_COUNT = 4
 PORT = 8080
 
 jar_target = 'target/chord-java-REST-1.0-jar-with-dependencies.jar'
 
-nodes_list = random.sample(range(CHORD_SIZE), TOTAL_NODES)
-present_nodes = []
-child_processes = []
+chord_size_list = [128, 256, 512]
 
-proc = subprocess.Popen(['java', '-jar', jar_target, '-p', str(PORT), '-m'])
-time.sleep(2)
+file = open("results.txt", "a")
 
-for i,node in enumerate(nodes_list):
-
-  if i == 0:
-    print("initial node: ", node)
-    http_cmd = "http://localhost:%d/%d/create" % (PORT, node)
-    print(http_cmd)
-    response = requests.get(http_cmd)
-    print(response)
-
-  else:
-    entry_node = random.choice(present_nodes)
-    opt = "-p %d -i %d --jh localhost --jp %d --ji %d"   \
-          % (8080+node, node, 8080+entry_node, entry_node)	
-    print(opt)
+for chord_size in chord_size_list:
+  for j in range(1,5):
+    total_nodes = int(chord_size * j / 10) #was 4
+    print("chord size: ", chord_size, " total nodes: ", total_nodes)
+    nodes_list = random.sample(range(chord_size), total_nodes)
+    present_nodes = []
     
-    print("joining node: ", node, " to node: ", entry_node)
-    http_cmd = "http://localhost:%d/%d/create" % (PORT, node)
-    print(http_cmd)
-    response = requests.get(http_cmd)
-    print("create response: ", response)
-    http_cmd = "http://localhost:%d/%d/join/%d" % (PORT, node, entry_node)
-    print(http_cmd)
-    response = requests.get(http_cmd)
-    print("join response: ", response)
+    proc = subprocess.Popen(['java', '-jar', jar_target, '-p', str(PORT), '-m', '-l', str(chord_size)])
+    time.sleep(2)
     
-  present_nodes.append(node)
-
-for i in range(QUERY_COUNT):
-  entry_node = random.choice(present_nodes)
-  find_suc_node = random.randint(0,CHORD_SIZE-1)
-  query = "http://localhost:%d/%d/find-successor/%d" % (PORT, entry_node, find_suc_node)
-  print("entry node: ", entry_node, "target node: ", find_suc_node)
-  response = requests.get(query)
-  resp = response.json()
-  print(resp)
-
-# with open("live_nodes.txt", "wb") as fp:
-#   pickle.dump(present_nodes, fp)
-
-proc.wait() 
+    for i,node in enumerate(nodes_list):
+    
+      if i == 0:
+        print("initial node: ", node)
+        http_cmd = "http://localhost:%d/%d/create" % (PORT, node)
+        print(http_cmd)
+        response = requests.get(http_cmd)
+        print(response)
+    
+      else:
+        entry_node = random.choice(present_nodes)
+#        print("joining node: ", node, " to node: ", entry_node)
+        http_cmd = "http://localhost:%d/%d/create" % (PORT, node)
+#        print(http_cmd)
+        response = requests.get(http_cmd)
+#        print("create response: ", response)
+        http_cmd = "http://localhost:%d/%d/join/%d" % (PORT, node, entry_node)
+#        print(http_cmd)
+        response = requests.get(http_cmd)
+#        print("join response: ", response)
+        
+      present_nodes.append(node)
+    
+    hops = []
+    
+    for i in range(chord_size):
+      entry_node = random.choice(present_nodes)
+      #find_suc_node = random.randint(0,chord_size-1)
+      query = "http://localhost:%d/%d/find-successor/%d" % (PORT, entry_node, i)
+      print("entry node: ", entry_node, "target node: ", i)
+      response = requests.get(query)
+      resp = response.json()
+      path_count = resp['pathCount']
+#      print("path count: ", path_count)
+      hops.append(path_count)
+    
+    std_dev_hops = statistics.pstdev(hops) 
+    mean_hops = statistics.mean(hops)
+    median_hops = statistics.median(hops)
+    
+    result = "Chord size: %d Node count: %d ---- Mean: %f Median: %f StdDev: %f\n"  % (chord_size, total_nodes, mean_hops, median_hops, std_dev_hops)
+    print(result)
+    file.write(result)    
+    #proc.wait()
+    proc.kill()
